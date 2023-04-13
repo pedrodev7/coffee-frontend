440b8c3f53556545cede10182bd6a6af46903f52
Pedro <pedrohenrique096k@gmail.com>
2023-04-13 15:34:38 -0300

    page update
    pagina para listar itens de um café

--- src/pages/ListarItensDoCafe/index.tsx
+++ src/pages/ListarItensDoCafe/index.tsx
@@ -4,58 +4,99 @@ import Button from 'react-bootstrap/Button';
 import axios from 'axios';
 import { BASE_URL } from '../../util/Request';
 import Form from 'react-bootstrap/esm/Form';
-
+import { useParams } from 'react-router-dom';
+import { addDays, format, isToday, subDays } from 'date-fns';
+import { Alert, Badge, InputGroup } from 'react-bootstrap';
+import { cpf } from 'cpf-cnpj-validator';
 
 interface ItemCafe {
 
     cliente: {
         nome: String;
+        cpf: string;
     }
     nomeDoProduto: String;
     diaDoCafeDaManha: number;
+    trouxe: Boolean;
 }
 
 function ListarItensDoCafe() {
     const [itemCafe, setItemCafe] = useState<ItemCafe[]>([]);
+    const { id } = useParams();
 
     useEffect(() => {
-        axios.get(`${BASE_URL}/itemCafe/2/exibirCafe`)
+        axios.get(`${BASE_URL}/itemCafe/${id}/exibirCafe`)
             .then(response => {
                 setItemCafe(response.data)
-            })
+            });
 
     }, []);
 
-    return (
-        <Table striped bordered hover>
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Nome</th>
-                    <th>Item</th>
-                    <th>Trouxe?</th>
-                    <th>Actions</th>
-                </tr>
-            </thead>
-            <tbody>
-                {itemCafe.map((itemcafe) => (
-                    <tr key={itemcafe.diaDoCafeDaManha}>
-                        <td>{ }</td>
-                        <td>{itemcafe.cliente.nome}</td>
-                        <td>{itemcafe.nomeDoProduto}</td>
-                        <td>
-                            <Form.Group className="mb-3" id="formGridCheckbox">
-                                <Form.Check type="checkbox"/>
-                            </Form.Group>
-                        </td>
-                        <td>
-                            <Button variant="danger" size="sm">EXCLUIR</Button>
-                        </td>
-                    </tr>
-                ))}
 
-            </tbody>
-        </Table>
+
+    const handleExcluirItem = async (nomeProduto: String) => {
+        try {
+            await axios.delete(`${BASE_URL}/itemCafe/${id}/${nomeProduto}/delete`);
+            alert('Removido com sucesso');
+            window.location.reload();
+        } catch (error) {
+            console.log(error);
+        }
+
+    }
+
+    const handlerTrazerItem = async (nomeProduto: String) => {
+        try {
+            await axios.put(`${BASE_URL}/itemCafe/${id}/${nomeProduto}/trazer`);
+            alert(`O Item: ${nomeProduto} Marcou presença no Café da Manhã de Hoje! Parabéns. =)`);
+            window.location.reload();
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    return (
+        <div>
+
+            <Button variant="success" size="sm" href={`/${id}/add-item-cafe`}>EU QUERO CONTRIBUIR !!!</Button>
+            <Table striped bordered hover>
+
+                <thead>
+                    <tr>
+                        <th>Nome</th>
+                        <th>CPF</th>
+                        <th>Item</th>
+                        <th>Data</th>
+                        <th>Trouxe?</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {itemCafe.map((itemcafe) => (
+                        <tr key={itemcafe.diaDoCafeDaManha}>
+                            <td>{itemcafe.cliente.nome}</td>
+                            <td>{cpf.format(itemcafe.cliente.cpf)}</td>
+                            <td>{itemcafe.nomeDoProduto}</td>
+                            <td>{format(addDays(new Date(itemcafe.diaDoCafeDaManha), 1), 'dd/MM/yyyy')}</td>                        
+                            <td>
+                                <Form.Group className="mb-3" id="formGridCheckbox">
+                                    <Form.Check
+                                        type="checkbox"
+                                        disabled={!isToday(subDays(new Date(itemcafe.diaDoCafeDaManha), -1))}
+                                        checked={itemcafe.trouxe === true}
+                                        onChange={(e) => handlerTrazerItem(itemcafe.nomeDoProduto)}
+                                    />
+                                </Form.Group>
+                            </td>
+                            <td>
+                                <Button variant="danger" size="sm" onClick={() => handleExcluirItem(itemcafe.nomeDoProduto)}>EXCLUIR</Button>
+                            </td>
+                        </tr>
+                    ))}
+
+                </tbody>
+            </Table>
+        </div>
     )
 }
 
